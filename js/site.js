756cc82d3649fff98380c1849702ae97ab16df94
Gelson Hernandez <gelsonpf49@hotmail.com>
2023-07-20 17:56:36 -0400

    Added for loop

--- js/site.js
+++ js/site.js
@@ -1,13 +1,50 @@
-function displayMessage() {
-  let inputBox = document.getElementById("message");
+// Get the numbers from our form inputs
+// ENTRY POINT
+// Controller Function
+function getValues() {
+  let startNumber = document.getElementById("startValue").value;
+  let endNumber = document.getElementById("endValue").value;
 
-  let message = inputBox.value;
+  startNumber = Number(startNumber);
+  endNumber = Number(endNumber);
 
-  Swal.fire({
-    backdrop: false,
-    title: "App Name",
-    text: message,
-  });
+  let numberArray = generateNumbers(startNumber, endNumber);
+
+  displayNumbers(numberArray);
 }
 
+// Business logic - creates every number in the input range
+// Data Model
+function generateNumbers(start, end) {
+  let range = [];
 
+  for (let number = start; number <= end; number = number + 1) {
+    range.push(number);
+  }
+
+  return range;
+}
+
+// put the numbers on the page
+// View Function
+function displayNumbers(numbersToDisplay) {
+  let tableHtml = "";
+
+  for (let index = 0; index < numbersToDisplay.length; index = index + 1) {
+    let currentNumber = numbersToDisplay[index];
+
+    let className = "";
+
+    if (currentNumber % 2 == 0) {
+      className = "even";
+    } else {
+      className = "odd";
+    }
+
+    let tableRowHtml = `<tr><td class="${className}">${currentNumber} </td></tr>`;
+
+    tableHtml = tableHtml + tableRowHtml;
+  }
+
+  document.getElementById("results").innerHTML = tableHtml;
+}
